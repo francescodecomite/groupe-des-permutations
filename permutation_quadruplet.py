0826b88b04208ca198179da1ae601eb5d4bd8e38
Francesco De Comité <francesco.de-comite@univ-lille.fr>
2026-05-20 16:53:43 +0200

    chargement initial
    pas fait le retournement du cercle pour les triangles bleus

---
+++ permutation_quadruplet.py
@@ -0,0 +1,63 @@
+# Engendrer tous les triangles avec la permutation au milieu
+TAILLE=500
+pourcentage=0.05
+from math import *
+from itertools import permutations
+
+# Entête du fichier SVG.
+def debut(c=TAILLE):
+    entete="<svg viewBox=\"0 0 "+str(2*c)+" "+str(2*c)+"\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+    pied="</svg>\n"
+    # Le nom du fichier SVG, 
+    image=open("permutations.svg","w")
+    image.write(entete)
+    return image
+
+# Fin du fichier SVG
+def fin(image):
+    pied="</svg>\n"
+    image.write(pied)
+    image.close()
+
+
+
+# ça peut toujours servir
+def ligne(debut,fin,transform="\"\""):
+     s="<line x1=\""+str(debut[0])+"\" y1=\""+str(debut[1])+"\" x2=\""+str(fin[0])+"\" y2=\""+str(fin[1])+"\" stroke=\"red\"   transform="+transform+" />\n"
+     return s
+    
+
+def equilateral(c=TAILLE,dx=0,dy=0,color="\"red\"", transform="\"\""):
+    hauteur=c*sqrt(3)/2
+    return "<polygon points=\""+str(dx)+" "+str(c+dy)+" ,"+str(c/2+dx)+" "+str(c-hauteur+dy)+" , "+str(c+dx)+" "+str(c+dy)+"\" fill=\"none\" stroke="+color+" transform="+transform+"/>\n"
+
+def texte(chaine,dx,dy):
+    
+    return("<text x=\""+str(dx)+"\" y=\""+str(dy)+"\" font-size=\"2em\">"+chaine+"</text>")
+    
+
+# Je connaissais pas, c'est Chatgpt qui me l'a dit
+def toutes_permutations(l):
+    k=list(permutations(l))
+   
+    return ["".join(map(str,u)) for u in k]
+
+
+if __name__=="__main__":
+    les_permuts= toutes_permutations([1,2,3,4])
+    taille=120   
+    image=debut()
+    for i in range(6):
+        for j in range(4):
+         ax=taille*i
+         ay=taille*j
+         image.write(equilateral(c=taille,color="\"green\"",dx=ax,dy=ay))
+         image.write("\n")
+         image.write(texte(les_permuts[6*j+i],dx=ax+taille*0.23,dy=ay+taille*0.75))
+         
+         
+    fin(image)
+    
+  
+
+
---
+++ permutation_quadruplet_version2.py
@@ -0,0 +1,81 @@
+# Engendrer tous les triangles avec la permutation écrite au milieu
+# version 2 : on essaie d'économiser le matériau
+
+TAILLE=500
+pourcentage=0.05
+from math import *
+from itertools import permutations
+
+# Entête du fichier SVG.
+def debut(c=TAILLE):
+    entete="<svg viewBox=\"0 0 "+str(2*c)+" "+str(2*c)+"\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+    pied="</svg>\n"
+    # Le nom du fichier SVG, 
+    image=open("permutationsV2.svg","w")
+    image.write(entete)
+    return image
+
+# Fin du fichier SVG
+def fin(image):
+    pied="</svg>\n"
+    image.write(pied)
+    image.close()
+
+
+
+# ça peut toujours servir
+def ligne(debut,fin,transform="\"\""):
+     s="<line x1=\""+str(debut[0])+"\" y1=\""+str(debut[1])+"\" x2=\""+str(fin[0])+"\" y2=\""+str(fin[1])+"\" stroke=\"red\"   transform="+transform+" />\n"
+     return s
+    
+
+def equilateral(c=TAILLE,dx=0,dy=0,color="\"red\"", transform="\"\""):
+    hauteur=c*sqrt(3)/2
+    return "<polygon points=\""+str(dx)+" "+str(c+dy)+" ,"+str(c/2+dx)+" "+str(c-hauteur+dy)+" , "+str(c+dx)+" "+str(c+dy)+"\" fill=\"none\" stroke="+color+" transform="+transform+"/>\n"
+
+def equilateral_envers(c=TAILLE,dx=0,dy=0,color="\"red\"", transform="\"\""):
+    hauteur=c*sqrt(3)/2
+    return "<polygon points=\""+str(dx)+" "+str(dy)+" ,"+str(c/2+dx)+" "+str(hauteur+dy)+" , "+str(c+dx)+" "+str(dy)+"\" fill=\"none\" stroke="+color+" transform="+transform+"/>\n"
+
+
+
+def texte(chaine,dx,dy):
+    
+    return("<text x=\""+str(dx)+"\" y=\""+str(dy)+"\" font-size=\"2em\">"+chaine+"</text>")
+    
+
+# Je connaissais pas, c'est Chatgpt qui me l'a dit
+def toutes_permutations(l):
+    k=list(permutations(l))
+   
+    return ["".join(map(str,u)) for u in k]
+
+
+if __name__=="__main__":
+    les_permuts= toutes_permutations([1,2,3,4])
+    taille=120
+    epsilon=5
+    epsy=5
+    image=debut()
+    for u in range(24):
+         i=u//4
+         j=u%4
+         ax=taille*i
+         ay=taille*j
+         if u%2==0:
+          image.write(equilateral(c=taille,color="\"green\"",dx=ax+i*epsilon,dy=ay/2))
+          image.write("\n")
+          image.write(texte(les_permuts[6*j+i],dx=ax+taille*0.23+i*epsilon,dy=ay/2+taille*0.75))
+          #image.write(texte(les_permuts[6*j+i],dx=ax+taille*0.23+i*epsilon,dy=ay/2+taille*0.75))
+         else :
+          image.write("\n")
+          image.write(equilateral_envers(c=taille,color="\"blue\"",dx=ax+taille/2+i*epsilon,dy=ay/2-taille/2+epsy))
+          image.write("\n")
+          #image.write(texte(les_permuts[6*j+i],dx=ax+taille*0.23,dy=ay+taille*0.75,transform=t))
+         
+         
+    fin(image)
+    
+  
+
+
