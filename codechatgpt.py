c7b328338f1842e844d7e1dd026ad1044f44040e
Francesco De Comité <francesco.de-comite@univ-lille.fr>
2026-06-04 14:02:08 +0200

    deux méthodes pour retrouver une rotation

---
+++ codechatgpt.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+
+A=np.array((-0.7,0.7,-0.7))
+B=np.array((0.7,-0.7,0.7))
+C=np.array((0.0,1.06,0.0))
+
+Aprime = np.array([0.383967774468709, -0.457122008769346, 1.489277869851005])
+Bprime = np.array([2.616032225531291, -1.142877991230654, 2.910722130148995])
+Cprime = np.array([0.944699164644112, -0.003743029127177, 2.625592486757558])
+
+
+def frame(A, B, C):
+    e1 = B - A
+    e1 = e1 / np.linalg.norm(e1)
+
+    v = C - A
+    v = v - np.dot(v, e1) * e1
+    e2 = v / np.linalg.norm(v)
+
+    e3 = np.cross(e1, e2)
+
+    return np.column_stack((e1, e2, e3))
+
+Q  = frame(A,  B,  C)
+Qp = frame(Aprime, Bprime, Cprime)
+
+R = Qp @ Q.T
+t = Aprime - R @ A
+print(t)
+
+
+
+print(Q)
+print(np.linalg.det(R))
+print(np.linalg.norm(R.T @ R - np.eye(3)))
+
--- matrices.py
+++ matrices.py
@@ -13,10 +13,16 @@ A=array((-0.7,0.7,-0.7))
 B=array((0.7,-0.7,0.7))
 C=array((0.0,1.06,0.0))
 
-
+"""
 Aprime=array((0.393,-0.45,1.49))
 Bprime=array((2.607,-1.15,2.91))
 Cprime=array((0.945,-0.004,2.625))
+"""
+
+Aprime = array([0.383967774468709, -0.457122008769346, 1.489277869851005])
+Bprime = array([2.616032225531291, -1.142877991230654, 2.910722130148995])
+Cprime = array([0.944699164644112, -0.003743029127177, 2.625592486757558])
+
 
 #Calcul de la matrice de rotation
 #Calcul de Q
@@ -24,23 +30,61 @@ u=B-A
 normeDeU=linalg.norm(u)
 e1=u/normeDeU
 
+
 #ok
 # Deuxième vecteur
 v=C-A
-print(A)
-print(v)
 alpha=dot(v,e1)
+
 #Ok
 vperp=v-alpha*e1
 
+
 normedevperp=linalg.norm(vperp)
+
 e2=vperp/normedevperp
+
 #ok
 e3=cross(e1,e2)
 # ok
 print("\n")
 Q=column_stack([e1,e2,e3])
-print(Q)
 
 
+# La matrice est orthogonale, c'est bon
+
+# On fait pareil sur le triangle final
+uprime=Bprime-Aprime
+normeDeUprime=linalg.norm(uprime)
+e1prime=uprime/normeDeUprime
+#ok
+
+vprime=Cprime-Aprime
+print(vprime)
+
+alpha=dot(vprime,e1prime)
+print(" **")
+print(alpha)
+print(" **")
+
+vperp=vprime-alpha*e1prime
+print(vperp)
+normedevperp=linalg.norm(vperp)
+print(normedevperp)
+e2prime=vperp/normedevperp
+
+e3prime=cross(e1prime,e2prime)
+print(e3prime)
+print(" ")
+Qprime=column_stack([e1prime,e2prime,e3prime])
+print(Qprime)
+
+
+resu=Qprime@Q.T
+
+print(resu)
+
+#La translation
+t=Aprime-resu@A
+print(t)
 
